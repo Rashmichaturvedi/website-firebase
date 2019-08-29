374ced80fd70d84bb2787552ec75026118972326
Rashmi chaturvedi <rc1981999@gmail.com>
2019-08-29 21:12:10 +0530

    Add files via upload

--- register.js
+++ register.js
@@ -15,35 +15,17 @@ function writeUserData(){
     });
 
 }
-database.ref ('users/').on('values',getData,errData);
-function getData(data){
-    var score=data.val();
-    var keys=object.keys(score);
-    for(var i=0;i<keys.length;i++){
-        var k=keys[i];
-        var frname=users[k].frname;
-        var mrname=users[k].mrname;
-        var lrname=users[k].lrname;
-        var pswrd=users[k].pswrd;
-        var li=CreateElement('li',frname+':'+mrname+':'+lrname+':'+pswrd);
-        document.getElementById('userlist').innerText=li;
-    }
-}
-function errData(data){
-    console.log("error");
-}
-/*database.ref('users/').once('value',function(snapshot)
+function ReadUserData(){
+ var htmldata ='';
+database.ref('users/').once('value',function(snapshot)
 {   snapshot.forEach(function(childSnapshot) {
     var userdata=childSnapshot.val();
-     var userkey=childSnapshot.key;
-     for(let key in users){
-        htmldata +='frname'+userkey+":"+userdata;
-
-     }
+      htmldata += "<li>" + userdata.frname +' : '+ userdata.mrname +' :  '+userdata.lrname +' : '+userdata.pswrd+ "</li>";     
+});
+document.getElementById('userlist').innerHTML=htmldata;
 });
 
+}
 
- document.getElementById('scorelist').innerText=htmldata;
-    });*/
 
 
